03952eb94c783191ff956424add7ffe8d9145be5
Duy Anh Nguyen <duyanh19962012@gmail.com>
2019-04-08 13:17:14 +0300

    Create alksdjkl.js

---
+++ alksdjkl.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+  const instance = axios.create({
+  baseURL: 'https://some-domain.com/api/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+});
+
+  axios.post('/user', {
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
